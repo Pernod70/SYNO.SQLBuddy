f211a8b1a3988d2de9f2543626323a5e989b6271
Pernod70 <ngbarnes@hotmail.com>
2014-02-15 21:02:47 +0000

    Initial commit.

---
+++ make.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+chown -R root:root desktop SQLBuddy ui
+chmod -R 444 *
+chmod -R a+X *
+chmod -R u+rw *
+tar -czf package.tgz -X exclude.txt desktop SQLBuddy ui
+
+chown -R root:root scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG
+chmod -R 755 *
+tar -cvf ../SQLBuddy-1.3.3-002.spk -X exclude.txt scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG
+
+exit 0
