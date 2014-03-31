10251958c101caca111f7436e4d3d349b52db72b
Pernod70 <ngbarnes@hotmail.com>
2014-03-31 22:03:10 +0100

    Fixed make.sh script.

--- src/make.sh
+++ src/make.sh
@@ -8,6 +8,6 @@ tar -czf package.tgz -X exclude.txt desktop SQLBuddy ui
 
 chown -R root:root scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 chmod -R 755 *
-tar -cvf ../SQLBuddy-1.3.3-002.spk -X exclude.txt scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
+tar -cvf ../spk/SQLBuddy-1.3.3-002.spk -X exclude.txt scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 
 exit 0
