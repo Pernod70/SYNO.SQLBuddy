5b39cc8bebf49bbe16674bc04f97ef5932a5f2f6
Pernod70 <ngbarnes@hotmail.com>
2014-03-31 01:16:01 +0100

    Updates for DSM 5.0 - Removed Web Station check.

--- src/make.sh
+++ src/make.sh
@@ -6,8 +6,8 @@ chmod -R a+X *
 chmod -R u+rw *
 tar -czf package.tgz -X exclude.txt desktop SQLBuddy ui
 
-chown -R root:root scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG
+chown -R root:root scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 chmod -R 755 *
-tar -cvf ../SQLBuddy-1.3.3-002.spk -X exclude.txt scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG
+tar -cvf ../SQLBuddy-1.3.3-002.spk -X exclude.txt scripts CHANGELOG LICENSE INFO package.tgz PACKAGE_ICON.PNG PACKAGE_ICON_120.PNG
 
 exit 0
